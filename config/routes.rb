d38e15ebb4c955cefd9d7d36bf61a0ef6a270590
Addison Martin <addisonejmartin@gmail.com>
2019-03-02 09:41:04 -0800

    Gave signup the boostrap treatment, changes its route to be just signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   devise_scope :user do
      get 'login', to: 'devise/sessions#new'
   end
+
+  devise_scope :user do
+     get 'signup', to: 'devise/registrations#new'
+  end
 end

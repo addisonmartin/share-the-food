2ae630574036b3671cde112f38bfaf66ab6a1f52
Addison Martin <addisonejmartin@gmail.com>
2019-03-03 22:10:30 -0800

    Fixed a bug where js wouldn't run. Made the card tiles prettier

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
 //= require jquery
 //= require bootstrap-sprockets
+
+//= require_tree .
--- app/assets/javascripts/donations/style.js
+++ app/assets/javascripts/donations/style.js
@@ -28,13 +28,13 @@ var colorSet = randomColorSet();
 var mode = Math.floor(Math.random() * 2);
 
 function randomColorSet() {
-   var colorSet1 = ['#000000', '#F0000F', '#0000FF', '#000FFF', '#00FFFF'];
-   var colorSet2 = ['#FFFFFF', '#000000', '#FFFFFF', '#000000', '#FFFFFF'];
-   var colorSet3 = ['#000000', '#00F000', '#000000', '#00F000', '#000000'];
+   var colorSet1 = ['#00F3F3', '#000FDA', '#0000FD', '#000DFF', '#00FFFF'];
+   var colorSet2 = ['#012012', '#000000', '#033232', '#332331', '#030300'];
+   var colorSet3 = ['#00F000', '#00F000', '#00DF00', '#00FD00', '#00AD00'];
    var colorSet4 = ['#000000', '#000000', '#0F0000', '#000000', '#0000F0'];
-   var colorSet5 = ['#F00000', '#000000', '#000000', '#00F000', '#000000'];
-   var colorSet6 = ['#000000', '#000000', '#0000F0', '#000000', '#000000'];
-   var volorSet7 = ['#000F00', '#000000', '#000000', '#000000', '#F00F00']
+   var colorSet5 = ['#F00000', '#F00000', '#F00000', '#FF9F9F', '#F00000'];
+   var colorSet6 = ['#9999D9', '#FFFFFF', '#FFFDFF', '#FFFDDF', '#FDFFFF'];
+   var colorSet7 = ['#D9FF9F', '#F9DF9F', '#F9DD9F', '#F9DDDD', '#DDFF9F']
 
    var randomSets = [colorSet1, colorSet2, colorSet3, colorSet4, colorSet5, colorSet6, colorSet7];
    var randomSet = randomSets[Math.floor(Math.random() * randomSets.length)];

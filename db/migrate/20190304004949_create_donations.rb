83f9f08b0b052ab296fb3b40f6b3a9e175aacc8c
Addison Martin <addisonejmartin@gmail.com>
2019-03-03 17:53:48 -0800

    Updated the donation model to include more details.

--- db/migrate/20190304004949_create_donations.rb
+++ db/migrate/20190304004949_create_donations.rb
@@ -1,10 +1,26 @@
 class CreateDonations < ActiveRecord::Migration[5.2]
   def change
     create_table :donations do |t|
-      t.string :name
-      t.text :details
-      t.belongs_to :region, index: true
-      t.belongs_to :user, index: true
+      t.string :name, required: true
+      t.text :details, required: true
+      t.datetime :fresh_until, required: true
+      t.float :latitude, required: true
+      t.float :longitude, required: true
+      t.text :pickup_notes, required: true
+      t.boolean :is_perishable, required: true
+      t.boolean :requires_preparation, required: true
+      t.boolean :is_vegetarian, required: true
+      t.boolean :is_vegan, required: true
+      t.boolean :is_gluten_free, required: true
+      t.boolean :contains_peanut, required: true
+      t.boolean :contains_treenut, required: true
+      t.boolean :contains_dairy, required: true
+      t.boolean :contains_soy, required: true
+      t.boolean :contains_egg, required: true
+      t.boolean :contains_fish, required: true
+      t.boolean :contains_shellfish, required: true
+      t.belongs_to :region, index: true, required: true
+      t.belongs_to :user, index: true, required: true
       t.timestamps
     end
   end

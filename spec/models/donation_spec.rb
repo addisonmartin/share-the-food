7315815214e7277f7c457e2125f1176f1c588ea1
Addison Martin <addisonejmartin@gmail.com>
2019-03-04 16:51:21 -0800

    Added test for search bar

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -19,5 +19,35 @@ RSpec.describe Donation, type: :model do
          second_donation = create(:donation)
          expect(Donation.all).to eq [second_donation, first_donation]
       end
+
+      it 'by_region scope gets donations from a particular region only' do
+         region = create(:region)
+         create(:donation, region_id: region.id)
+         create_list(:donation, 10)
+         donations = Donation.by_region(region.name)
+         expect(donations.count).to eq 1
+         expect(donations[0].region.name).to eq region.name
+      end
+
+      it 'by_city scope gets donations from a particular city only' do
+         region = create(:region, city: "Morgantown")
+         create(:donation, region_id: region.id)
+         create_list(:donation, 10)
+         donations = Donation.by_city(region.name, region.city)
+         expect(donations.count).to eq 1
+         expect(donations[0].region.city).to eq region.city
+      end
+
+      it 'search scope finds matching donations (by name and details) only' do
+         donation = create(:donation, name: 'addison martin', details: 'cal poly')
+         create_list(:donation, 10)
+         expect(Donation.search('addison martin').count).to eq 1
+         expect(Donation.search('addison martin')[0].id).to eq donation.id
+         expect(Donation.search('cal poly').count).to eq 1
+         expect(Donation.search('cal poly')[0].id).to eq donation.id
+
+         expect(Donation.search('bob').count).to eq 0
+         expect(Donation.search('james').count).to eq 0
+      end
    end
 end

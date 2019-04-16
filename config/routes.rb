facef8a65a5d522f046581ed69008e9e012337b3
Unknown <addisonejmartin@gmail.com>
2019-04-16 01:29:35 -0700

    Wrapped signup in bootstraps grid system.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :business_users, path: 'business_users', controllers: { sessions: "business_users/sessions"}
-  devise_for :users, path: 'users', controllers: { sessions: "users/sessions" }
+  devise_for :business_users, path: 'business_users', controllers: { sessions: "business_users/sessions", registrations: "business_users/registrations"}
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions", registrations: "users/registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#index'

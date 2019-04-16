d6e072ef9ce71468e45eb6c20ce28341b2bc6e76
Unknown <addisonejmartin@gmail.com>
2019-04-16 01:50:58 -0700

    Bug fix. Updated accessible concern, which checks between users, to be public

--- app/controllers/business_users/sessions_controller.rb
+++ app/controllers/business_users/sessions_controller.rb
@@ -1,6 +1,3 @@
-
-# frozen_string_literal: true
-
 class BusinessUsers::SessionsController < Devise::SessionsController
    include Accessible
 
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
    include Accessible
 

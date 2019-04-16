dc0eca89b887f1d3f3b973935a52d78281bf79e5
Unknown <addisonejmartin@gmail.com>
2019-04-16 05:12:11 -0700

    Added a view for donations on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ include Accessible
 class PagesController < ApplicationController
 
    def index
+     @donations = Donation.limit(10)
    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#index'
+
+  resource :donations do
+
+  end
 end

706078341a047e2f0ec6f43fd936625cfe1ff518
Unknown <addisonejmartin@gmail.com>
2019-04-18 20:13:11 -0700

    Added a friendly message when there are no donations nearby.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 require 'navigation_helper.rb'
+require 'donations_helper.rb'
 
 module ApplicationHelper
    include NavigationHelper
+   include DonationsHelper
 end
--- app/helpers/donations_helper.rb
+++ app/helpers/donations_helper.rb
@@ -1,2 +1,7 @@
 module DonationsHelper
+
+  def no_donations_partial_path
+    @donations.empty? ? 'donations/no_donations' : 'shared/empty_partial'
+  end
+
 end

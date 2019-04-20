876ed4317f55c68f699c41f02813f1b21e7e0c06
Unknown <addisonejmartin@gmail.com>
2019-04-20 09:13:34 -0700

    Readded upload donation, now refractored

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,5 +1,8 @@
 class DonationsController < ApplicationController
 
+  # Ensures users who are not signed in CANNOT make a new donation.
+  before_action :redirect_if_not_signed_in, only: [:new]
+
   def show
     @donation = Donation.find(params[:id])
   end
@@ -14,6 +17,27 @@ class DonationsController < ApplicationController
       @donations = Donation.search(search).paginate(page: params[:page], per_page: 20)
     end
 
+  end
+
+  def new
+    @donation = Donation.new
+  end
+
+  def create
+    @donation = Donation.new(donation_params)
+
+    if @donation.save
+      redirect_to donation_path(@donation)
+    else
+      redirect_to root_path
+    end
+
+  end
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:name, :description, :fresh_until, :latitude, :longitude, :pickup_notes, :is_perishable, :requires_preparation, :is_vegetarian, :is_vegan, :is_gluten_free, :contains_peanut, :contains_treenut, :contains_dairy, :contains_soy, :contains_egg, :contains_fish, :contains_shellfish).merge(user_id: current_user.id)
   end
 
 end

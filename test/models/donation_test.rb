15d7abfb49a1af4c87092656872c0ff924db6eeb
Unknown <addisonejmartin@gmail.com>
2019-04-20 18:42:50 -0700

    Updated the donation model test

--- test/models/donation_test.rb
+++ test/models/donation_test.rb
@@ -31,127 +31,287 @@ require 'test_helper'
 class DonationTest < ActiveSupport::TestCase
 
   test "should not save donation without a user" do
-    donation = donations(:default_user_donation)
-    donation.user = nil
+    donation = Donation.new
 
     assert_not donation.save, "Saved the donation without a user."
   end
 
   test "should not save donation without a name" do
-    donation = donations(:default_user_donation)
-    donation.name = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
 
     assert_not donation.save, "Saved the donation without a name."
   end
 
   test "should not save donation without a description" do
-    donation = donations(:default_user_donation)
-    donation.description = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
 
     assert_not donation.save, "Saved the donation without a description."
   end
 
   test "should not save donation without a fresh until date" do
-    donation = donations(:default_user_donation)
-    donation.fresh_until = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
 
     assert_not donation.save, "Saved the donation without a fresh until date."
   end
 
   test "should not save donation without a latitude" do
-    donation = donations(:default_user_donation)
-    donation.latitude = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
 
     assert_not donation.save, "Saved the donation without a latitude."
   end
 
   test "should not save donation without a longitude" do
-    donation = donations(:default_user_donation)
-    donation.longitude = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
 
     assert_not donation.save, "Saved the donation without a longitude."
   end
 
   test "should not save donation without pick up notes" do
-    donation = donations(:default_user_donation)
-    donation.pickup_notes = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
 
     assert_not donation.save, "Saved the donation without pick up notes."
   end
 
   test "should not save donation without a is perishable value" do
-    donation = donations(:default_user_donation)
-    donation.is_perishable = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
+    donation.pickup_notes = "Pickup notes"
 
     assert_not donation.save, "Saved the donation without a is perishable value."
   end
 
   test "should not save donation without a requires preparation value" do
-    donation = donations(:default_user_donation)
-    donation.requires_preparation = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
+    donation.pickup_notes = "Pickup notes"
+    donation.is_perishable = true
 
     assert_not donation.save, "Saved the donation without a requires preparation value."
   end
 
   test "should not save donation without a is vegetarian value" do
-    donation = donations(:default_user_donation)
-    donation.is_vegetarian = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
+    donation.pickup_notes = "Pickup notes"
+    donation.is_perishable = true
+    donation.requires_preparation = true
 
     assert_not donation.save, "Saved the donation without a is vegetarian value."
   end
 
   test "should not save donation without a is vegan value" do
-    donation = donations(:default_user_donation)
-    donation.is_vegan = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
+    donation.pickup_notes = "Pickup notes"
+    donation.is_perishable = true
+    donation.requires_preparation = true
+    donation.is_vegetarian = true
 
     assert_not donation.save, "Saved the donation without a is vegan value."
   end
 
   test "should not save donation without a is gluten free value" do
-    donation = donations(:default_user_donation)
-    donation.is_gluten_free = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
+    donation.pickup_notes = "Pickup notes"
+    donation.is_perishable = true
+    donation.requires_preparation = true
+    donation.is_vegetarian = true
+    donation.is_vegan = true
 
     assert_not donation.save, "Saved the donation without a is gluten free value."
   end
 
   test "should not save donation without a contains peanut value" do
-    donation = donations(:default_user_donation)
-    donation.contains_peanut = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
+    donation.pickup_notes = "Pickup notes"
+    donation.is_perishable = true
+    donation.requires_preparation = true
+    donation.is_vegetarian = true
+    donation.is_vegan = true
+    donation.is_gluten_free = true
 
     assert_not donation.save, "Saved the donation without a contains peanut value."
   end
 
   test "should not save donation without a contains treenut value" do
-    donation = donations(:default_user_donation)
-    donation.contains_treenut = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
+    donation.pickup_notes = "Pickup notes"
+    donation.is_perishable = true
+    donation.requires_preparation = true
+    donation.is_vegetarian = true
+    donation.is_vegan = true
+    donation.is_gluten_free = true
+    donation.contains_peanut = true
 
     assert_not donation.save, "Saved the donation without a contains treenut value."
   end
 
+  test "should not save donation without a contains dairy value." do
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
+    donation.pickup_notes = "Pickup notes"
+    donation.is_perishable = true
+    donation.requires_preparation = true
+    donation.is_vegetarian = true
+    donation.is_vegan = true
+    donation.is_gluten_free = true
+    donation.contains_peanut = true
+    donation.contains_treenut = true
+
+    assert_not donation.save, "Saved the donation without a contains dairy value."
+
+  end
+
   test "should not save donation without a contains soy value" do
-    donation = donations(:default_user_donation)
-    donation.contains_soy = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
+    donation.pickup_notes = "Pickup notes"
+    donation.is_perishable = true
+    donation.requires_preparation = true
+    donation.is_vegetarian = true
+    donation.is_vegan = true
+    donation.is_gluten_free = true
+    donation.contains_peanut = true
+    donation.contains_treenut = true
+    donation.contains_dairy = true
 
     assert_not donation.save, "Saved the donation without a contains soy value."
   end
 
   test "should not save donation without a contains egg value" do
-    donation = donations(:default_user_donation)
-    donation.contains_egg = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
+    donation.pickup_notes = "Pickup notes"
+    donation.is_perishable = true
+    donation.requires_preparation = true
+    donation.is_vegetarian = true
+    donation.is_vegan = true
+    donation.is_gluten_free = true
+    donation.contains_peanut = true
+    donation.contains_treenut = true
+    donation.contains_dairy = true
+    donation.contains_soy = true
 
     assert_not donation.save, "Saved the donation without a contains egg value."
   end
 
   test "should not save donation without a contains fish value" do
-    donation = donations(:default_user_donation)
-    donation.contains_fish = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
+    donation.pickup_notes = "Pickup notes"
+    donation.is_perishable = true
+    donation.requires_preparation = true
+    donation.is_vegetarian = true
+    donation.is_vegan = true
+    donation.is_gluten_free = true
+    donation.contains_peanut = true
+    donation.contains_treenut = true
+    donation.contains_dairy = true
+    donation.contains_soy = true
+    donation.contains_egg = true
 
     assert_not donation.save, "Saved the donation without a contains fish value."
   end
 
   test "should not save donation without a contains shellfish value" do
-    donation = donations(:default_user_donation)
-    donation.contains_shellfish = nil
+    donation = Donation.new
+    donation.user = users(:default_user)
+    donation.name = "Donation"
+    donation.description = "Description"
+    donation.fresh_until = Time.now
+    donation.latitude = 0
+    donation.longitude = 0
+    donation.pickup_notes = "Pickup notes"
+    donation.is_perishable = true
+    donation.requires_preparation = true
+    donation.is_vegetarian = true
+    donation.is_vegan = true
+    donation.is_gluten_free = true
+    donation.contains_peanut = true
+    donation.contains_treenut = true
+    donation.contains_dairy = true
+    donation.contains_soy = true
+    donation.contains_egg = true
+    donation.contains_fish = true
 
     assert_not donation.save, "Saved the donation without a contains shellfish value."
   end

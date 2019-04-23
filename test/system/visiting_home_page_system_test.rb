276d9f36fa375b26b5de861417ae191235d0969f
Unknown <addisonejmartin@gmail.com>
2019-04-22 18:31:03 -0700

    Merge

--- test/system/visiting_home_page_system_test.rb
+++ test/system/visiting_home_page_system_test.rb
@@ -2,10 +2,38 @@ require "application_system_test_case"
 
 class VisitingHomePageTest < ApplicationSystemTestCase
 
-  test "home page should have a button view and a button to add donations" do
+  test "home page should have a button to view and a button to add donations" do
     visit root_path
     assert_selector "button", text: "I want some food"
     assert_selector "button", text: "I have extra food"
+  end
+
+  test "clicking I want some food button should take you to view donations page" do
+    visit root_path
+    click_button "I want some food"
+
+    assert_selector "h1", text: "Nearby Donations"
+
+  end
+
+  test "viewing the default donations" do
+    visit root_path
+    click_button "I want some food"
+
+    assert_text "Default Business User Donation"
+    assert_text "Default User Donation"
+    assert_text "Donated by Default User"
+    assert_text "This is a donation. It was donated, by a user. ???"
+
+    assert_selector "button", text: "View Donation"
+
+  end
+
+  test "clicking I have extra food, and not signed in, should take you to a why signup page" do
+    visit root_path
+    click_button "I want some food"
+
+    assert_selector "h1", text: "Nearby Donations"
   end
 
 end

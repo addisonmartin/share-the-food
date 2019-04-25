d0ac4829bda2d617ebaa3cceb4d1dc65a684ea17
Addison Martin <addisonejmartin@gmail.com>
2019-04-25 15:38:19 -0700

    Added map view of donations with markers when viewing list of all donations

---
+++ app/assets/javascripts/donations/home_page.js
@@ -0,0 +1,27 @@
+function initMapForList(donations) {
+
+  var lat = 52.5;
+  var lng = 0.125;
+
+  if (donations.length != 0) {
+    lat = donations[0].latitude;
+    lng = donations[0].longitude;
+  }
+
+  var coords = new google.maps.LatLng(lat, lng);
+
+  var mapOptions = {
+    center: coords,
+    zoom: 14
+  };
+
+  var map = new google.maps.Map(document.getElementById('map3'), mapOptions);
+
+  for (i = 0; i < donations.length; i++) {
+    var markerCoords = new google.maps.LatLng(donations[i].latitude, donations[i].longitude);
+    var marker = new google.maps.Marker({
+      position: markerCoords,
+      map: map
+    });
+  }
+}

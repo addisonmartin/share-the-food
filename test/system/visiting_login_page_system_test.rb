d3de3b2d25caf358409d698fa38c44d3ba516da9
Addison Martin <addisonejmartin@gmail.com>
2019-04-27 09:24:46 -0700

    Updates to user test fixture

--- test/system/visiting_login_page_system_test.rb
+++ test/system/visiting_login_page_system_test.rb
@@ -2,4 +2,9 @@ require "application_system_test_case"
 
 class VisitingLoginPageTest < ApplicationSystemTestCase
 
+  test "logging in with valid credentials" do
+    visit root_path
+    
+  end
+
 end

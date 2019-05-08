e4bc2b0ccc478e1604880ea0b10c9428f7dc34f0
Addison Martin <addisonejmartin@gmail.com>
2019-05-08 00:38:33 -0700

    Reenabled navbar links switching depending if user signed in/out

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
 # The helper for navigation.
-# Used to change the links based on if user is signed in/out.
 module NavigationHelper
-  def collapsible_list_partial_path
-    if user_signed_in?
-      'layouts/navigation/collapsible_list/user_links'
-    else
-      'layouts/navigation/collapsible_list/not_signed_in_links'
-    end
-  end
 end

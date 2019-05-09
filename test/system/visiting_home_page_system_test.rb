719d8281aafabe5c64b56fc7664c12ef2caa9d1d
Addison Martin <addisonejmartin@gmail.com>
2019-05-08 17:11:28 -0700

    Changed footer text and style. Added tests for footer. Fixed navbar tests.

--- test/system/visiting_home_page_system_test.rb
+++ test/system/visiting_home_page_system_test.rb
@@ -20,9 +20,16 @@ class VisitingHomePageTest < ApplicationSystemTestCase
     assert_selector 'a', text: 'Add a Donation'
   end
 
+  test 'home page should have a footer' do
+    visit root_path
+
+    assert_selector 'footer', text: 'Share the Food | Questions? Contact us at: contact@sharethefood.net'
+  end
+
   test 'home page and not signed in, should have navigation bar links to sign up and in' do
     visit root_path
-    click_on 'dropdown'
+
+    click_link 'Account Settings'
 
     assert_selector 'a', text: 'Sign Up'
     assert_selector 'a', text: 'Log In'

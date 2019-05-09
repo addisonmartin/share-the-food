c2ef2eac01571b97617708446512347f46ee5cb5
Addison Martin <addisonejmartin@gmail.com>
2019-05-08 17:24:40 -0700

    Finished reimplementing view donations tests post gui refresh

--- test/system/visiting_view_donations_page_system_test.rb
+++ test/system/visiting_view_donations_page_system_test.rb
@@ -3,4 +3,26 @@
 require 'application_system_test_case'
 
 class VisitingViewDonationsPageTest < ApplicationSystemTestCase
+
+  test 'viewing donations from home page' do
+    visit root_path
+    click_link 'Find Donations'
+
+    assert_selector 'h1', text: 'View Donations'
+  end
+
+  test 'view donations on an embedded Google maps' do
+    visit root_path
+    click_link 'Find Donations'
+
+    assert_selector 'div', id: 'map3'
+  end
+
+  test 'view donations in a list view' do
+    visit root_path
+    click_link 'Find Donations'
+
+    assert_selector 'div', id: 'donations-list'
+  end
+
 end

48edc016b4254bdcdb52e36ec8ad6e7f1985fc4f
Addison Martin <addisonejmartin@gmail.com>
2019-05-10 13:32:57 -0700

    Fixed typo in new donation form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Does not have a model associated with it.
-# Used to controll the (view) why signup page.
+# Used to control the (view) why signup page.
 class PagesController < ApplicationController
   def show
     render file: 'pages/why_signup'
